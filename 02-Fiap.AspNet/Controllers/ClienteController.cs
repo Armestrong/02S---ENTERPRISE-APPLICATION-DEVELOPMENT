277b381adcc5607925ca013489badca7009cd345
Logon Aluno <logonaluno@local.com>
2019-08-15 09:50:21 -0300

    ASPNET

---
+++ 02-Fiap.AspNet/Controllers/ClienteController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _02_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_Fiap.Web.AspNet.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] // Abrir a pagina do form 
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+
+        [HttpPost] // Cadastar no clique do botao
+        public IActionResult Cadastrar(Cliente cliente)
+        {
+            // Envia info para a tela
+            ViewData["nome"] = cliente.Nome;
+            ViewBag.cpf = cliente.Cpf;
+            TempData["msg"] = "Usuario Cadastrodo";
+            return View();
+        }
+    }
+}
