a93a09bac88fa92dc1a485d36d07a5cddc1b2ab1
Logon Aluno <logonaluno@local.com>
2019-08-26 11:39:15 -0300

    tagHelpers

---
+++ 05-Fiap.Web.AspNet/05-Fiap.Web.AspNet/Controllers/ZooController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _05_Fiap.Web.AspNet.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Fiap.Web.AspNet.Controllers
+{
+    public class ZooController : Controller
+    {
+
+
+        private static IList<Zoo> _lista = new List<Zoo>();
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return View(_lista);
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(Zoo zoo)
+        {
+            _lista.Add(zoo);
+            TempData["msg"] = "Cadastrado com sucesso";
+            return RedirectToAction("Listar");
+        }
+    }
+}
